6bf83c4f096d0a096007f01439536be54e8d0061
quyetlv <m.quyetlv@gmail.com>
2024-12-11 22:39:38 +0700

    Set up project

---
+++ HotelBokingService/Connection/SqlConnectionFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace HotelBokingService.Connection
+{
+    public class SqlConnectionFactory
+    {
+        private readonly IConfiguration _configuration;
+
+        public SqlConnectionFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public SqlConnection CreateConnection()
+        {
+            return new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
---
+++ HotelBokingService/Models/APIResponse.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+
+namespace HotelBokingService.Models
+{
+    public class APIResponse<T>
+    {
+        public bool Success { get; set; }
+
+        public HttpStatusCode StatusCode { get; set; }
+
+        public string Message { get; set; }
+
+        public T Data { get; set; }
+
+        public object Error { get; set; }
+
+        public APIResponse(T data, string message = "", HttpStatusCode statusCode = HttpStatusCode.OK) 
+        {
+            Success = true;
+            StatusCode = statusCode;
+            Message = message;
+            Data = data;
+            Error = null;
+        }
+
+        public APIResponse(HttpStatusCode statusCode, string message, object error = null) 
+        {
+            Success = false;
+            StatusCode = statusCode;
+            Message = message;
+            Data = default(T);
+            Error = error;
+        }
+    }
+}
